593e602d7bd1f0b074692c8e4632d1ceb2962256
Jerinsamj <151986308+Jerinsamj@users.noreply.github.com>
2024-01-11 15:22:58 -0500

    Add files via upload

---
+++ Bing MAP api/js/arenas.js
@@ -0,0 +1,69 @@
+const pushpinlocationarenas = [
+  {
+    "OBJECTID": 1,
+    "NAME": "Beverly Arena",
+    "ADDRESS": "680 Highway 8",
+    "COMMUNITY": "Flamborough ",
+    "CATEGORY": "Arenas",
+    "COMMENTS": "",
+    "PHONE": "519-647-2722",
+    "LONGITUDE": -80.11132698230543,
+    "LATITUDE": 43.29850975800341
+  },
+  {
+    "OBJECTID": 2,
+    "NAME": "Bill Friday Lawfield Arena",
+    "ADDRESS": "150 Folkstone Avenue",
+    "COMMUNITY": "Hamilton ",
+    "CATEGORY": "Arenas",
+    "COMMENTS": "",
+    "PHONE": "905-546-4923",
+    "LONGITUDE": -79.85075118048759,
+    "LATITUDE": 43.21484012102844
+  },
+  {
+    "OBJECTID": 3,
+    "NAME": "Carlisle Arena",
+    "ADDRESS": "1496 Centre Road",
+    "COMMUNITY": "Flamborough ",
+    "CATEGORY": "Arenas",
+    "COMMENTS": "",
+    "PHONE": "905-689-7283",
+    "LONGITUDE": -79.98131996076191,
+    "LATITUDE": 43.39677849605811
+  },
+  {
+    "OBJECTID": 4,
+    "NAME": "Chedoke Twin Pad Arena",
+    "ADDRESS": "91 Chedmac Drive",
+    "COMMUNITY": "Hamilton ",
+    "CATEGORY": "Arenas",
+    "COMMENTS": "",
+    "PHONE": "905-546-2429",
+    "LONGITUDE": -79.92059943158375,
+    "LATITUDE": 43.238669620839
+  },
+  {
+    "OBJECTID": 5,
+    "NAME": "Coronation Arena",
+    "ADDRESS": "81 Macklin Street North",
+    "COMMUNITY": "Hamilton ",
+    "CATEGORY": "Arenas",
+    "COMMENTS": "",
+    "PHONE": "905-546-3109",
+    "LONGITUDE": -79.89626047530253,
+    "LATITUDE": 43.264644122018204
+  },
+  {
+    "OBJECTID": 6,
+    "NAME": "Eastwood Arena",
+    "ADDRESS": "111 Burlington Street East",
+    "COMMUNITY": "Hamilton ",
+    "CATEGORY": "Arenas",
+    "COMMENTS": "",
+    "PHONE": "905-546-3152",
+    "LONGITUDE": -79.85632979848626,
+    "LATITUDE": 43.27241885012524
+  }
+  
+]
---
+++ Bing MAP api/js/recreation.js
@@ -0,0 +1,57 @@
+const pushpinlocationrecreation = [
+    {
+    "OBJECTID": 7,
+    "NAME": "Carlisle Memorial Hall",
+    "ADDRESS": "",
+    "COMMUNITY": "Hamilton ",
+    "CATEGORY": "Recreation community centre",
+    "COMMENTS": "",
+    "PHONE": "905-546-4923",
+    "LONGITUDE": -79.98147985966365,
+    "LATITUDE": 43.3939428965672
+  },
+  {
+    "OBJECTID": 8,
+    "NAME": "Ryerson Recreation Centre",
+    "ADDRESS": "",
+    "COMMUNITY": "Hamilton ",
+    "CATEGORY": "Recreation community centre",
+    "COMMENTS": "",
+    "PHONE": "905-546-4923",
+    "LONGITUDE": -79.8823564426346,
+    "LATITUDE": 43.25381278759484
+  },
+  {
+    "OBJECTID": 9,
+    "NAME": "Flamborough Seniors Centre",
+    "ADDRESS": "",
+    "COMMUNITY": "Hamilton ",
+    "CATEGORY": "Recreation community centre",
+    "COMMENTS": "",
+    "PHONE": "905-546-4923",
+    "LONGITUDE": -79.90491465621322,
+    "LATITUDE": 43.32371612302919
+  },
+  {
+    "OBJECTID": 10,
+    "NAME": "Eastmount Community Centre",
+    "ADDRESS": "",
+    "COMMUNITY": "Hamilton ",
+    "CATEGORY": "Recreation community centre",
+    "COMMENTS": "",
+    "PHONE": "905-546-4923",
+    "LONGITUDE": -79.84866004361709,
+    "LATITUDE": 43.2364337165834
+  },
+  {
+    "OBJECTID": 11,
+    "NAME": "David Braley Athletic & Recreation Centre - Mohawk College",
+    "ADDRESS": "",
+    "COMMUNITY": "Hamilton ",
+    "CATEGORY": "Recreation community centre",
+    "COMMENTS": "",
+    "PHONE": "905-546-4923",
+    "LONGITUDE": -79.88851988342181,
+    "LATITUDE": 43.23863344326221
+  }
+]
